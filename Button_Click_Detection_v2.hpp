05e834af1a41aeac1bd6e31dd3aa4a4dbe04d1e1
Anxin Canada <75194970+AnxinCanada@users.noreply.github.com>
2023-01-28 16:26:52 -0800

    Added -1 Offset
    Forgot to offset by -1 due to C++ counting from 0 instead of 1 - Fixed.

--- Button_Click_Detection_v2.hpp
+++ Button_Click_Detection_v2.hpp
@@ -1,11 +1,11 @@
  bool button_values[5] = {True, True, True, True, True}; 
  bool check_for_button_click(int io) {
-     if (digitalRead(io) == LOW && not button_values[io - 10]) {
-         button_values[io - 10] = True;
+     if (digitalRead(io) == LOW && not button_values[io - 10 - 1]) {
+         button_values[io - 10 - 1] = True;
          return true;
      }
      else if (digitalRead(io) == HIGH) {
-         button_values[io - 10] = False;
+         button_values[io - 10 - 1] = False;
          return false;
      }
  }
